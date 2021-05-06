fc1e875725e33eddec88adc8838b35b8b5ea19c6
Helen Nelson <hnelson@fandm.edu>
2021-05-06 02:17:34 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,5 +28,5 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array
 }
 function removeElementFromEndOfArray(array) {
-  array.slice(array.length - 1)
+  array.slice(0, (array.length - 1))
 }
